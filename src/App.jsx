c2dd00b8a37a06700e2d4263fe1e16575f4b59dd
Bennett Diaz <97626907+bennett-diaz@users.noreply.github.com>
2024-08-09 14:49:57 -0500

    Gluestack working

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import Results from './components/Results'; 
+import Results from './components/Results';
+import { GluestackUIProvider, Box, Button, ButtonText } from '@gluestack-ui/themed';
+import { config } from "./styles/gluestack_config/gluestack-ui.config"
 
 
 function App() {
     console.log('hello world')
 
     return (
+        <GluestackUIProvider config={config}>
         <div>
             <h1>Hello, Instacap!</h1>
             <Results />
+            <Button>
+                <ButtonText>Hello world</ButtonText>
+            </Button>
         </div>
+        </GluestackUIProvider>
     );
 }
 
